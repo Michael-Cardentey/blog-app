ba9e296d5e5bb5686580432d237bfd467f9e03c4
Michael Cardentey <cardenteym07@gmail.com>
2024-03-07 16:36:20 -0500

    First commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+   <Link href="/">
+   <h1>hello hello hello hello hello hello </h1>
+    Hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    hello hello hello hello hello hello hello hello hello 
+    </Link>
+  );
 }
