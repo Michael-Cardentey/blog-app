85ab0b3df8a144d60f995972c2e1a2c88fc4a4f5
Michael-Cardentey <cardenteym07@gmail.com>
2024-03-14 13:11:08 -0400

    CSS dark mode theme toggle design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,17 @@
-import Link from "next/link";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
 import styles from "./homepage.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-   <Link href="/">
-   <h1>hello hello hello hello hello hello </h1>
-    Hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    hello hello hello hello hello hello hello hello hello 
-    </Link>
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+      </div>
+    </div>
   );
 }
