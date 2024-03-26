dd2e4f60e30df87474c06424e51bba07561325d1
Michael-Cardentey <cardenteym07@gmail.com>
2024-03-26 15:29:49 -0400

    Blog category page design

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -10,8 +10,8 @@ const Card = () => {
         </div>
         <div className={styles.textContainer}>
             <div className={styles.detail}>
-                <span className={styles.date}>03.19.2024</span>
-                <span className={styles.category}>CULTURE</span>
+                <span className={styles.date}>03.19.2024 </span>
+                <span className={styles.category}> CULTURE</span>
             </div>
             <Link href="/">
                 <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius doloremque ullam illo similique. Quod, non?</h1>
